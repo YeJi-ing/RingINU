9df231ec0580bfafaea257cc6026065b201b27f8
YeJi <ping_yeji@naver.com>
2022-12-26 20:17:12 +0900

    add welcome page

---
+++ src/main/java/RingINU/Ringspring/RingSpringApplication.java
@@ -0,0 +1,13 @@
+package RingINU.Ringspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RingINU/Ringspring/RingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package RingINU.Ringspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
