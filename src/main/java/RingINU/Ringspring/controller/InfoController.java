f7a33ce607ffc401cccd570ffa113ff9ebbcea8c
YeJi <ping_yeji@naver.com>
2022-12-30 05:55:42 +0900

    Final

--- src/main/java/RingINU/Ringspring/controller/InfoController.java
+++ src/main/java/RingINU/Ringspring/controller/InfoController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
 
-
 @Controller
 public class InfoController {
     // 현재 진행 중인 사용자 ID
@@ -20,7 +19,8 @@ public class InfoController {
         ResultSet rs = null;
 
         Class.forName("org.sqlite.JDBC");
-        String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
+        //String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
+        String dbFileUrl = "./src/main/java/RingINU/Ringspring/controller/DB/UserInfo.db";
 
         try{
             con=DriverManager.getConnection("jdbc:sqlite:" + dbFileUrl);
@@ -45,7 +45,8 @@ public class InfoController {
         ResultSet rs = null;
 
         Class.forName("org.sqlite.JDBC");
-        String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
+        //String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
+        String dbFileUrl = "./src/main/java/RingINU/Ringspring/controller/DB/UserInfo.db";
 
         try{
             con=DriverManager.getConnection("jdbc:sqlite:" + dbFileUrl);
@@ -61,30 +62,6 @@ public class InfoController {
             //rs.close();
         }
     }
-
-//    void SortDB() throws ClassNotFoundException, SQLException {
-//        Connection con = null;
-//        Statement stmt = null;
-//        ResultSet rs = null;
-//
-//        Class.forName("org.sqlite.JDBC");
-//        String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
-//
-//        try{
-//            con=DriverManager.getConnection("jdbc:sqlite:" + dbFileUrl);
-//            System.out.println("DB connected");
-//
-//            stmt=con.createStatement();
-//            stmt.executeUpdate("insert into info(userID, score) values(\"" + userID + "\",null);");
-//            // 0을 null로 바꿈
-//
-//        }catch (Exception e) {
-//            System.out.println(e);
-//        }finally {
-//            stmt.close();
-//            //rs.close();
-//        }
-//    }
 
 
     @RequestMapping(value = "send", method = RequestMethod.GET)
@@ -114,5 +91,84 @@ public class InfoController {
         return "page3";
     }
 
+
+    @GetMapping("situp") // 객체를 그대로 넘기는 상황 => JsonConverter 동작
+    public String situpApi(Model model) throws SQLException, ClassNotFoundException {
+        String nickname = ID;
+
+        score = 300; // 임시 스코어 값
+        UpdateDB(score);
+
+        model.addAttribute("score", score);
+        model.addAttribute("nickname", nickname);
+
+        return "page3_su";
+    }
+
+    @GetMapping("pushup") // 객체를 그대로 넘기는 상황 => JsonConverter 동작
+    public String pushupApi(Model model) throws SQLException, ClassNotFoundException {
+        String nickname = ID;
+
+        score = 300; // 임시 스코어 값
+        UpdateDB(score);
+
+        model.addAttribute("score", score);
+        model.addAttribute("nickname", nickname);
+
+        return "page3_ps";
+    }
+
+    @GetMapping("result")
+    public String resultApi(Model model) throws SQLException, ClassNotFoundException {
+
+        String[] top = new String[3];
+        int[] ScoreNum = new int[3];
+
+        // SortDB
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        Class.forName("org.sqlite.JDBC");
+        //String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
+        String dbFileUrl = "./src/main/java/RingINU/Ringspring/controller/DB/UserInfo.db";
+
+        try{
+            con=DriverManager.getConnection("jdbc:sqlite:" + dbFileUrl);
+            System.out.println("DB connected");
+
+            stmt=con.createStatement();
+            rs = stmt.executeQuery("SELECT userID, score FROM info ORDER by score desc LIMIT 3;");
+
+            int cnt = 3;
+
+            for(int i =0;i<cnt;i++)
+            {
+                rs.next();
+                top[i] = rs.getString("userID");
+                ScoreNum[i] = rs.getInt("score");
+            }
+
+
+        }catch (Exception e) {
+            System.out.println(e);
+        }finally {
+            stmt.close();
+            rs.close();
+        }
+
+        model.addAttribute("userID", ID);
+        model.addAttribute("userScore", score);
+
+        model.addAttribute("top1", top[0]);
+        model.addAttribute("top2", top[1]);
+        model.addAttribute("top3", top[2]);
+
+        model.addAttribute("score1", ScoreNum[0]);
+        model.addAttribute("score2", ScoreNum[1]);
+        model.addAttribute("score3", ScoreNum[2]);
+
+        return "page4";
+    }
 
 }
