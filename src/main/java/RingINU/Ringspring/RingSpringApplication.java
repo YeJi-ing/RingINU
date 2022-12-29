261157a994fd0a9284fdce5511a89e6c866703cc
YeJi <ping_yeji@naver.com>
2022-12-29 13:53:17 +0900

    test file

--- src/main/java/RingINU/Ringspring/RingSpringApplication.java
+++ src/main/java/RingINU/Ringspring/RingSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RingSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RingSpringApplication.class, args);
 	}
 
