16b43fb64748d8475e3f314891d8b00bb7179a3d
YeJi <ping_yeji@naver.com>
2022-12-30 01:15:23 +0900

    기본 기능 구현

--- src/main/java/RingINU/Ringspring/controller/InfoController.java
+++ src/main/java/RingINU/Ringspring/controller/InfoController.java
@@ -6,23 +6,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
 
-import java.io.*;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Objects;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 
 @Controller
 public class InfoController {
-    @RequestMapping(value = "/send", method = RequestMethod.GET)
-    public String send(String userID, Model model) throws ClassNotFoundException, SQLException {
-        model.addAttribute("userID", userID);
+    // 현재 진행 중인 사용자 ID
+    String ID;
+    int score;
 
-        // 입력한 userID을 DB에 저장
+    // 입력한 userID을 DB에 저장 ( setAutoCommit(true) 상태 )
+    void InsertDB(String userID) throws ClassNotFoundException, SQLException {
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
@@ -30,50 +22,73 @@ public class InfoController {
         Class.forName("org.sqlite.JDBC");
         String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
 
-        //try{
+        try{
             con=DriverManager.getConnection("jdbc:sqlite:" + dbFileUrl);
             System.out.println("DB connected");
 
             stmt=con.createStatement();
-            stmt.executeUpdate("insert into info(userID, score) values(\"" + userID + "\",0);");
-
-            //rs=stmt.executeQuery("");
-            con.commit();
-            con.setAutoCommit(true);
-        //}//catch (Exception e) {
-            //System.out.println(e);
-       // }finally {
-            con.close();
+            stmt.executeUpdate("insert into info(userID, score) values(\"" + userID + "\",null);"); 
+            // 0을 null로 바꿈
+            
+        }catch (Exception e) {
+            System.out.println(e);
+        }finally {
             stmt.close();
-            rs.close();
-        //}
+            //rs.close();
+        }
+    }
+
+    // 최종 score를 DB에 저장
+    void UpdateDB(int score) throws ClassNotFoundException, SQLException {
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        Class.forName("org.sqlite.JDBC");
+        String dbFileUrl = "C:\\Users\\HanYeJi\\Desktop\\jetbot\\Ring-spring\\src\\main\\java\\RingINU\\Ringspring\\controller\\DB\\UserInfo.db";
+
+        try{
+            con=DriverManager.getConnection("jdbc:sqlite:" + dbFileUrl);
+            System.out.println("DB connected");
+
+            stmt=con.createStatement();
+            stmt.executeUpdate("UPDATE info set score=" + score + " where userID=\"" + ID + "\";");
+
+        }catch (Exception e) {
+            System.out.println(e);
+        }finally {
+            stmt.close();
+            //rs.close();
+        }
+
+    }
+    
+    @RequestMapping(value = "send", method = RequestMethod.GET)
+    public String send(String userID, Model model) throws ClassNotFoundException, SQLException {
+        model.addAttribute("userID", userID);
+
+        // 입력한 UserID를 DB에 저장
+        InsertDB(userID);
+
+        // 진행중인 사용자 ID 업데이트
+        ID = userID;
 
         return "testinfo";
-        return
     }
 
-    @GetMapping("hello-string") // http://localhost:8080/hello-string?name=예지
-    @ResponseBody // 내가 직접 html body 부분에 해당내용을 넣겠다 == 기본 문자 그대로 넘겨라 => StringConverter 동작
-    public String helloString(@RequestParam("name") String name){
-        return "hello " + name; // name: spring => "hello spring"
-    }
+    // page2에서 호출, page3로
+    @GetMapping("squat") // 객체를 그대로 넘기는 상황 => JsonConverter 동작
+    public String helloApi(Model model) throws SQLException, ClassNotFoundException {
+        String nickname = ID;
+        
+        score = 300; // 임시 스코어 값 
+        UpdateDB(score);
 
-    // API 방식: view 없이 HttpMessageConverter(StringConverter or JsonConverter) 동작. return 문자 그대로 내려감
-//    @GetMapping("hello-string") // http://localhost:8080/hello-string?name=
-//    @ResponseBody // 내가 직접 html body 부분에 해당내용을 넣겠다 == 기본 문자 그대로 넘겨라 => StringConverter 동작
-//    public String helloString(@RequestParam("name") String name){
-//        JSONParser parser = new JSONParser();
-//        String jsonFilename = "/timetable.json";
-//
-//        File file = new File(jsonFilename);
-//        FileReader fileReader1 = new FileReader(file);
-//        Object obj = parser.parse(fileReader1);
-//
-//        JSONObject jsonObject =(JSONObject) obj;
-//        System.out.println("============"+jsonObject.toString());
-//
-//        return "hello " + name; // name: spring => "hello spring"
-//    }
+        model.addAttribute("score", score);
+        model.addAttribute("nickname", nickname);
+
+        return "page3";
+    }
 
 
 }
