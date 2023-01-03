48aa898a58a3a4898b86bf77bdd57d96b57b5423
ChoiYJ2 <chldb1221@naver.com>
2023-01-03 16:14:00 +0900

    Add python folder

---
+++ src/main/resources/templates/python/cal_sitUp.py
@@ -0,0 +1,123 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+cap = cv2.VideoCapture(0)
+
+# Curl counter variables
+counter = 0 
+stage = None
+
+# def calculate_angle(a,b,c):
+#     a = np.array(a) # First
+#     b = np.array(b) # Mid
+#     c = np.array(c) # End
+    
+#     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+#     angle = np.abs(radians*180.0/np.pi)
+    
+#     if angle >180.0:
+#         angle = 360-angle
+        
+#     return angle
+    
+def cal_sitUP(ls, lh, lk):
+    l_s = np.array(ls)
+    l_h = np.array(lh)
+    l_k = np.array(lk)
+    radi = np.arctan2(l_k[1] - l_h[1], l_k[0] - l_h[0]) - np.arctan2(l_s[1] - l_h[1], l_s[0] - l_h[0])
+    cal = np.abs(radi*180.0/np.pi)
+    
+    if cal > 180.0:
+        cal = 360 - cal
+    return cal
+
+## Setup mediapipe instance
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            # Get coordinates
+            # r_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            l_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            l_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+            # r_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            l_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+            l_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            # r_sholder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOLDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOLDER.value].y]
+            
+            # Calculate angle
+            # angle = calculate_angle(l_shoulder, elbow, wrist)
+            angle = cal_sitUP(l_shoulder, l_hip, l_knee)
+            
+            # Visualize angle
+            cv2.putText(image, str(angle), 
+                           tuple(np.multiply(l_hip, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            # cv2.putText(image, str(angle), 
+            #                tuple(np.multiply(r_knee, [640, 480]).astype(int)), 
+            #                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            # Curl counter logic
+            if angle > 100:
+                stage = "down"
+            if angle < 60 and stage == "down":
+                stage="up"
+                counter +=1
+                print(stage)
+                print(counter)
+                       
+        except:
+            pass
+        
+        # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0,0), (70,50), (255, 255, 255), -1)
+        
+        # Rep data
+        cv2.putText(image, 'Reps', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (25,45), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        
+        # Stage data
+        # cv2.putText(image, 'STAGE', (65,12), 
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        # cv2.putText(image, stage, 
+        #             (60,60), 
+        #             cv2.FONT_HERSHEY_SIMPLEX, 2, (), 2, cv2.LINE_AA)
+        
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
